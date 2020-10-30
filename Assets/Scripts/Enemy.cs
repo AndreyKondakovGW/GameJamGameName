a49d89c8db82e5fbcce0166712323046f0702ca8
elia-morrison <andsimof@gmail.com>
2020-10-30 12:23:29 +0300

    Health bar

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float attackSpeed;
+    public float health;
+    public float maxHealth;
+
+    protected abstract void UpdateAI();
+    protected abstract void OnHit(GameObject player);
+    protected abstract void OnLostTrack();
+    protected abstract void OnDeath();
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int HP;
+    public int maxHP;
+
+    public UIHealthBar HB;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HB.UpdateHP(HP);
+        HB.UpdateMaxHP(maxHP, HP);
+    }
+}
